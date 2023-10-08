cb4c36b712b550ba6ac7462bf525e4f2a2d91f5e
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-10-07 20:10:41 -0400

    Compleated new K7 Challenge

---
+++ kyu7/Printer Errors/script.js
@@ -0,0 +1,14 @@
+//Testing 
+    var s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+    console.log(printerErrors(s)) //Expected Result "3/56"
+
+//Script
+function printerErrors(s){
+    return `${s.length - s.replace(/[^a-m]/g,"").length}/${s.length}`;
+}
+
+//Creative Solution
+//Using an arrow function ot reduce to a single line
+//Count the valid characters rather then the invalid characters
+//Accounts for case insensitvity
+const printerError = s => `${s.replace(/[a-m]/gi, "").length}/${s.length}`;
